b1955318477e2c7ac06e313073d78acb4c56dcd4
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-06-24 19:51:40 -0300

    configuraçao de variaveis .ENV com chaves da API e configuraçao de requisiçoes

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ov3509198b01xk2s6md6j6/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
